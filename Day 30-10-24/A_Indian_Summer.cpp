4a3e867dff7eb9b56a44d9f332077bc57f4647af
Apurbo04 <154694784+apurbo04@users.noreply.github.com>
2024-10-31 06:06:54 +0600

    Update A_Indian_Summer.cpp
    updated

--- Day 30-10-24/A_Indian_Summer.cpp
+++ Day 30-10-24/A_Indian_Summer.cpp
@@ -8,32 +8,22 @@ int main()
 
      int n;
      cin >> n;
-     vector<pair<string, string>> v;
-     for (int i = 0; i < n; i++)
+     map<pair<string, string>, bool> mp;
+     for (int i = 1; i <= n; i++)
      {
-          string tree, color;
-          cin >> tree >> color;
-          v.push_back({tree, color});
+          string a, b;
+          cin >> a >> b;
+          mp[{a, b}] = true;
      }
-     int cnt2 = 0;
-     bool flag = false;
-     for (int i = 0; i < n - 1; i++)
-     {
-          for (int j = i + 1; j < n; j++)
-          {
-               if (v[i].first == v[j].first && v[i].second == v[j].second)
-               {
-                    if (flag)
-                         continue;
-                    flag = true;
-               }
-               else if (v[i].first != v[j].first && v[i].second != v[j].second)
-                    cnt2++;
-          }
-     }
-     if (flag)
-          cout << 1;
-     else
-          cout << cnt2;
+
+     // for (auto it : mp)
+     // {
+     //      pair<string, string> p = it.first;
+     //      bool cnt = it.second;
+     //      // cout << p.first << " " << p.second << " " << cnt << endl;
+     //      cout << it.first.first << " " << it.first.second << " " << it.second<<endl;
+     // }
+
+     cout<<mp.size();
      return 0;
 }
