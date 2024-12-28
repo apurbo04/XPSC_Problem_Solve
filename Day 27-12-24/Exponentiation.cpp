aa3a8e5d94f43805eadb958812c845b5bbb26efb
Apurbo04 <154694784+apurbo04@users.noreply.github.com>
2024-12-28 22:47:48 +0600

    Update Exponentiation.cpp

--- Day 27-12-24/Exponentiation.cpp
+++ Day 27-12-24/Exponentiation.cpp
@@ -4,15 +4,32 @@
 #define ot cout <<
 #define no cout << "NO\n"
 #define yes cout << "YES\n"
-#define vt int n;cin>>n;vector<int> v(n)
+#define vt     \
+     int n;    \
+     cin >> n; \
+     vector<int> v(n)
 #define nd << '\n'
 
 using namespace std;
+const int MOD = 1e9 + 7;
+
+int power(int x, int n)
+{
+     int ans = 1 % MOD;
+     while (n)
+     {
+          if (n & 1)
+               ans = (1LL * ans % MOD * x % MOD) % MOD;
+          x = 1LL * x * x % MOD;
+          n >>= 1;
+     }
+     return ans;
+}
 void solve()
 {
-   int a,b;
-   in a>>b;
-   
+     int a, b;
+     in a >> b;
+     ot power(a, b) nd;
 }
 int main()
 {
